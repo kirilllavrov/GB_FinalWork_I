0fddb6df3dfdde861022254727b8c5c78850d279
Kirill Lavrov <kirill.lavrov@checloud.ru>
2023-01-04 01:47:38 +0300

    Added a condition for filling a new array

--- Task/Program.cs
+++ Task/Program.cs
@@ -10,3 +10,19 @@ for (int i = 0; i < Names.Length; i++)
     }
 }
 
+string[] NamesSort = new string[count];
+int j = 0;
+
+for (int i = 0; i < Names.Length; i++)
+{
+    if (Names[i].Length <= 3)
+    {
+        NamesSort[j++] = Names[i];
+    }
+}
+
+
+for (int i = 0; i < NamesSort.Length; i++)
+{
+    Console.WriteLine(NamesSort[i]);
+}
